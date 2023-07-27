16687e205f7fb97b6b6aff50e83c1e5534e1b604
K.D. Hubbard <hubbard.kd@gmail.com>
2023-07-27 17:07:50 -0600

    add ingredient validations and associations

--- spec/models/ingredient_spec.rb
+++ spec/models/ingredient_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Ingredient, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do 
+    it { should validate_presence_of :name }
+    it { should validate_presence_of :units }
+    it { should validate_presence_of :unit_type }
+    it { should validate_numericality_of :units }
+  end
+
+  describe "associations" do 
+    it { should have_many :recipe_ingredients }
+    it { should have_many(:recipes).through(:recipe_ingredients) }
+  end
 end

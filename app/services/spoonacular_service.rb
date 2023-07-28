d9799f493a989300453eb152a2a377cd27817bae
K.D. Hubbard <hubbard.kd@gmail.com>
2023-07-28 12:07:15 -0600

    add spoonacular service

--- app/services/spoonacular_service.rb
+++ app/services/spoonacular_service.rb
@@ -1,7 +1,17 @@
 class SpoonacularService
   def conn
     Faraday.new(url: "https://api.spoonacular.com/recipes/") do |faraday|
-      faraday.params["api_key"] = ENV["SPOON-KEY"]
+      faraday.params["apiKey"] = ENV["SPOON-KEY"]
     end
+  end
+
+  def get_url(url)
+    response = conn.get(url)
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def recipes_by_ingredients(ingr_array)
+    search = ingr_array.join(",+")
+    get_url("findByIngredients?ingredients=#{search}")
   end
 end
---
+++ spec/services/spoonacular_service_spec.rb
@@ -0,0 +1,23 @@
+require "rails_helper" 
+
+RSpec.describe SpoonacularService do 
+  describe "instance methods" do 
+    describe "#recipes_by_ingredients" do 
+      it "gets all recipes based on list of ingredients", :vcr  do 
+        query = ["salt, potatoes"]
+        search = SpoonacularService.new.recipes_by_ingredients(query)
+        
+        expect(search).to be_an(Array)
+        
+        recipe = search.first
+
+        expect(recipe).to have_key(:usedIngredientCount)
+        expect(recipe).to have_key(:usedIngredients)
+        expect(recipe[:usedIngredients]).to be_an(Array)
+        expect(recipe[:usedIngredients].first).to have_key(:name)
+        expect(recipe[:usedIngredients].first).to have_key(:amount)
+        expect(recipe[:usedIngredients].first).to have_key(:unitShort)
+      end
+    end
+  end
+end

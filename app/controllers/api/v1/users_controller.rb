8ebd1efc034f9265a026f69a92c75a9f8dcf31d3
Michael Callahan <mcalla242@gmail.com>
2023-07-27 18:55:35 -0600

    Add functionality to support Create One User post requests

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,4 +2,13 @@ class Api::V1::UsersController < ApplicationController
   def show
     render json: UserSerializer.new(User.find(params[:id]))
   end
+
+  def create
+    render json: UserSerializer.new(User.create!(user_params)), status: :created
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:uid, :name, :email, :token)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :users, only: %i[show]
+      resources :users, only: %i[show create]
     end
   end
 end

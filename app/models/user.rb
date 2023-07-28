6a44a2f205253ad4715f9eba0e9926cd8b800702
K.D. Hubbard <hubbard.kd@gmail.com>
2023-07-28 08:48:01 -0600

    remove token from users

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,4 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :email, presence: true
   validates :email, uniqueness: true
-  validates :token, presence: true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,6 +6,5 @@ RSpec.describe User, type: :model do
     it { should validate_presence_of :name }
     it { should validate_presence_of :email }
     it { should validate_uniqueness_of :email }
-    it { should validate_presence_of :token }
   end
 end

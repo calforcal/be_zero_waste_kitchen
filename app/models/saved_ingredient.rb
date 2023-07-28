d99e92f26bcb2cff0ecae899e97515adcabe918d
K.D. Hubbard <hubbard.kd@gmail.com>
2023-07-27 21:38:26 -0600

    add saved ingredients validations and associations

--- app/models/saved_ingredient.rb
+++ app/models/saved_ingredient.rb
@@ -1,3 +1,7 @@
 class SavedIngredient < ApplicationRecord
   belongs_to :user
+  validates :ingredient_name, presence: true
+  validates :unit_type, presence: true
+  validates :units, presence: true
+  validates :units, numericality: true 
 end
--- spec/models/saved_ingredient_spec.rb
+++ spec/models/saved_ingredient_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe SavedIngredient, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do 
+    it { should validate_presence_of :ingredient_name }
+    it { should validate_presence_of :unit_type }
+    it { should validate_presence_of :units }
+    it { should validate_numericality_of :units }
+  end
+
+  describe "associations" do 
+    it { should belong_to :user }
+  end
 end

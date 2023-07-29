13c7a222f9b1a43525b58fc58cde3388b275d5a3
Michael Callahan <mcalla242@gmail.com>
2023-07-29 10:53:36 -0600

    Update serializer to pass through more attributes for a User

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,4 +1,24 @@
 class UserSerializer
   include JSONAPI::Serializer
   attributes :uid, :name, :email
+
+  attribute :stats do |object|
+    object.user_stats
+  end
+
+  attribute :cooked_recipes do |object|
+    object.recipes_cooked
+  end
+
+  attribute :created_recipes do |object|
+    object.recipes_created
+  end
+
+  attribute :num_cooked_recipes do |object|
+    object.num_recipes_cooked
+  end
+
+  attribute :num_created_recipes do |object|
+    object.num_recipes_created
+  end
 end

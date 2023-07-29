04955069ca168716369e634ab70cd19287cbec4f
K.D. Hubbard <hubbard.kd@gmail.com>
2023-07-29 09:45:14 -0600

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,13 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-
+  
   namespace :api do
     namespace :v1 do
       resources :users, only: %i[show create]
+      resources :recipes 
+      resources :ingredients, only: %i[index]
+      get "/recipes/search", to: "search#index"
     end
   end
 end

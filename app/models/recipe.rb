0e7ef803eb032ff1ea77aff229c34edbfffb7e8d
K.D. Hubbard <hubbard.kd@gmail.com>
2023-07-29 20:00:02 -0600

    wip search by ingredients

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -11,12 +11,23 @@ class Recipe < ApplicationRecord
   end
 
   def self.ingredient_search_details(ingredients)
-    term1 = joins(:ingredients).where(:ingredients => {:name => ingredients[0]})
-    term2 = joins(:ingredients).where(:ingredients => {:name => ingredients[1]})
-    term3 = joins(:ingredients).where(:ingredients => {:name => ingredients[2]})
-    term1.merge(term2).merge(term3)
+    term_results = []
+    ingredients.each do |ingredient|
+      term_results << joins(:ingredients).where(:ingredients => {:name => ingredient})
+    end
+    first_result = term_results[0]
+    all_other_results = term_results.map do |result|
+      merge(result)
+    end
+    require 'pry'; binding.pry
+    first_result.all_other_results
   end
 end
+
+# term1 = joins(:ingredients).where(:ingredients => {:name => ingredients[0]})
+# term2 = joins(:ingredients).where(:ingredients => {:name => ingredients[1]})
+# term3 = joins(:ingredients).where(:ingredients => {:name => ingredients[2]})
+# term1.merge(term2).merge(term3)
 
 # joins(:ingredients).where(:ingredients => {:name => ingredients })
 #     joins(:ingredients).where(:ingredients => {:name => "sauce" }).and(Recipe.joins(:ingredients).where(:ingredients => {:name => "pasta" }))

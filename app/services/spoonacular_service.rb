12c29e0e1379f1bc4fee42885d9b465ccc9bfba6
K.D. Hubbard <hubbard.kd@gmail.com>
2023-07-30 00:17:21 -0600

    add limit 10 results to spoonacular service

--- app/services/spoonacular_service.rb
+++ app/services/spoonacular_service.rb
@@ -2,6 +2,7 @@ class SpoonacularService
   def conn
     Faraday.new(url: 'https://api.spoonacular.com/recipes/') do |faraday|
       faraday.params['apiKey'] = ENV['SPOON-KEY']
+      faraday.params['number'] = 10
     end
   end
 

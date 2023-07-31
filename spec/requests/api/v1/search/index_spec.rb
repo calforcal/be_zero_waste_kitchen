526e32aa8594eed91b1d34d61e085b043b33dedd
K.D. Hubbard <hubbard.kd@gmail.com>
2023-07-30 20:28:39 -0600

    add search by name test and stub

--- spec/requests/api/v1/search/index_spec.rb
+++ spec/requests/api/v1/search/index_spec.rb
@@ -13,6 +13,7 @@ RSpec.describe "Recipes Search", type: :request do
           }
         )
       .to_return(status: 200, body: eggplant_search, headers: {})
+      
       get "/api/v1/recipes/search?ingredients=eggplant"
 
       expect(response).to be_successful
@@ -21,6 +22,35 @@ RSpec.describe "Recipes Search", type: :request do
 
       recipes = parsed[:data]
 
+      expect(recipes[0][:id]).to be_a(String)
+      expect(recipes[0][:type]).to eq("search")
+      expect(recipes[0][:attributes]).to be_an(Hash)
+      expect(recipes[0][:attributes][:name]).to be_a(String)
+      expect(recipes[0][:attributes][:api_id]).to be_a(String)
+    end
+  end
+  
+  describe "get recipes by name" do 
+    it "can get a list of recipes with name search" do 
+      eggplant_search = File.read('spec/fixtures/search_index_name.json')
+      stub_request(:get, "/api/v1/recipes/search?ingredients=eggplant")
+        .with(
+          headers: {
+            'Accept' => '*/*',
+            'Accept-Encoding' => 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+            'User-Agent' => 'Faraday v2.7.9'
+          }
+        )
+      .to_return(status: 200, body: eggplant_search, headers: {})
+
+      get "/api/v1/recipes/search?name=eggplant"
+    
+      expect(response).to be_successful
+    
+      parsed = JSON.parse(response.body, symbolize_names: true)
+    
+      recipes = parsed[:data]
+    
       expect(recipes[0][:id]).to be_a(String)
       expect(recipes[0][:type]).to eq("search")
       expect(recipes[0][:attributes]).to be_an(Hash)

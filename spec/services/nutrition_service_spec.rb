f6c0f385881b468b719eac25d414e79227f475d2
K.D. Hubbard <hubbard.kd@gmail.com>
2023-07-31 09:29:24 -0600

    add vcr tag back to nutrition service test

--- spec/services/nutrition_service_spec.rb
+++ spec/services/nutrition_service_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe NutritionService do
   describe 'instance methods' do
     describe '#ingredients weight' do
-      it 'can take in a string of ingredients and return nutrition facts'do
+      it 'can take in a string of ingredients and return nutrition facts', :vcr do
         query = '2 lbs chicken'
         search = NutritionService.new.ingredients_weight(query)
         expect(search).to be_an(Array)

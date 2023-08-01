accb0dfc5c9213bc64c9474433bbfa3656371769
K.D. Hubbard <hubbard.kd@gmail.com>
2023-07-31 19:35:18 -0600

    small refactor in controller

--- app/controllers/api/v1/user_recipes_controller.rb
+++ app/controllers/api/v1/user_recipes_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::UserRecipesController < ApplicationController
   def create
-    user = User.find_by(uid: params[:uid])
+    user = find_user(params)
     user_recipe = UserRecipe.find_by(user_id: user.id, recipe_id: user_recipe_params[:recipe_id])
     if !user_recipe
       user.user_recipes.create!(user_recipe_params)
@@ -10,13 +10,17 @@ class Api::V1::UserRecipesController < ApplicationController
   end
     
   def update
-    user = User.find_by(uid: params[:uid])
+    user = find_user(params)
     user_recipe = UserRecipe.find_by(user_id: user.id, recipe_id: params[:recipe_id])
     # move logic of if not_cooked, can't rate to front end.
     user_recipe.update(user_recipe_update_params)
   end
 
   private 
+
+  def find_user(params)
+    User.find_by(uid: params[:uid])
+  end
 
   def user_recipe_params
     params.permit(:user_id, :recipe_id, :cook_status, :saved_status, :is_owner)

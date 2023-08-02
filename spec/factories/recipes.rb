4e63c79f1af81d1f6084fbf0d381a186e5e41f3d
Matthew Johnson <matthewwmjohnson@gmail.com>
2023-08-02 17:33:54 -0600

    edit recipe factory.

--- spec/factories/recipes.rb
+++ spec/factories/recipes.rb
@@ -1,11 +1,11 @@
 FactoryBot.define do
   factory :recipe do
-    name { 'MyString' }
-    instructions { 'MyString' }
-    image_url { 'MyString' }
+    name { 'Banana Foster' }
+    instructions { ",1. Eat,2. Enjoy," }
+    image_url { 'banana_foster_image.com' }
     cook_time { 1 }
     public_status { true }
-    source_name { 'MyString' }
-    source_url { 'MyString' }
+    source_name { 'Chef Mateo' }
+    source_url { 'www.chefmateo.com' }
   end
 end

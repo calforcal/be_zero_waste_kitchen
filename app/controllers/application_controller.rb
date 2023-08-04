01938a58b1cf724a2e81182c574a4c26b898d784
Michael Callahan <118235615+calforcal@users.noreply.github.com>
2023-08-03 20:04:34 -0600

    Set up backend API validation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,16 @@ class ApplicationController < ActionController::API
   rescue_from ActiveRecord::RecordNotFound, with: :error_response
   rescue_from ActiveRecord::RecordInvalid, with: :error_response
 
+  before_action :validate_api_key
+
+
   def error_response(error)
     render json: ErrorSerializer.new(error).serialize_json, status: :not_found
+  end
+
+  def validate_api_key
+    if ENV['BACKEND_API_KEY'] != params[:zwk_api_key]
+      render json: { message: "Bad credentials" }, status: :unauthorized
+    end
   end
 end
